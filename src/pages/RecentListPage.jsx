6cf33228bfc4310641ab86b7e14382050cc7f6b1
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-08-13 17:55:33 +0900

    refac: RecentListPage의 state에서 PopupVisible 삭제

--- src/pages/RecentListPage.jsx
+++ src/pages/RecentListPage.jsx
@@ -11,7 +11,6 @@ class RecentListPage extends Component {
       clickedItems: [],
       filteredItems: [],
       selectedBrands: [],
-      popupVisible: false,
       checkBoxState: false,
     };
   }
